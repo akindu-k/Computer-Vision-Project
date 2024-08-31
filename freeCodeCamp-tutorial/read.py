78dfebdb4553e593180ecf24dd681ed22ea701e4
akindu-k <akinduk619@gmail.com>
2024-08-31 09:32:32 +0530

    fine tuning video and image reading

--- freeCodeCamp-tutorial/read.py
+++ freeCodeCamp-tutorial/read.py
@@ -1,18 +1,20 @@
 import cv2 as cv
 
-#img = cv.imread("Photos/ep1.png")
-
-#cv.imshow("Episode 1", img)
-
-
-capture = cv.VideoCapture(0)
+capture = cv.VideoCapture("Videos/Discrete Time Convolution.mp4")
 
 while True:
     isTrue, frame = capture.read()
+    
+    if not isTrue:
+        print("Failed to grab frame or end of video reached")
+        break
+    
     cv.imshow("Video", frame)
 
-    if (cv.waitKey(20) & oxFF==ord("d")):
+    
+    if cv.waitKey(20) & 0xFF == ord("d"):
         break
+
 
 capture.release()
 cv.destroyAllWindows()
